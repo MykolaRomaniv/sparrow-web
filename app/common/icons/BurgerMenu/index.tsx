000c6e80cf9f9c6e200b00a45dc1aaee90fa8d41
Mykola Romaniv <mykolaromaniw@gmail.com>
2022-02-20 16:55:39 +0200

    feat: add burger menu icon

---
+++ app/common/icons/BurgerMenu/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./BurgerMenu.module.scss";
+
+const BurgerMenu = () => (
+  <div className={styles.container}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default BurgerMenu;
